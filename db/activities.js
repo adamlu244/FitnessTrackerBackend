a998bae7ff4c8e999c41f165c079d48641b1b96e
Adam Lu <adamlu09@gmail.com>
2023-02-02 19:20:22 -0800

    activities db mostly done, just needs attachAllActivitiesToRoutines function

--- db/activities.js
+++ db/activities.js
@@ -3,6 +3,9 @@ const client = require('./client');
 // database functions
 async function createActivity({ name, description }) {
   // return the new activity
+  // had to change test specs for this function so that all names are lowercased
+  // test spec expected Marathon and not marathon, but instructions said to make 
+  // it lowercase.
   try {
     const { rows: [ activity ] } = await client.query(
       `INSERT INTO activities(name, description)
@@ -31,18 +34,60 @@ async function getAllActivities() {
   }
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  try {
+    const { rows: [ activity ] } = await client.query(
+      `SELECT *
+      FROM activities
+      WHERE id=$1;`
+    , [id])
 
-async function getActivityByName(name) {}
+    return activity;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getActivityByName(name) {
+  try {
+    const { rows: [ activity ] } = await client.query(
+      `SELECT *
+      FROM activities
+      WHERE name=$1;`
+    , [name])
+
+    return activity;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function attachActivitiesToRoutines(routines) {
   // select and return an array of all activities
+  // will need this for all get methods in routine functions
+  // Save for later?
 }
 
 async function updateActivity({ id, ...fields }) {
   // don't try to update the id
   // do update the name and description
   // return the updated activity
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  try {
+    const { rows: [ activity ] } = await client.query(
+      `UPDATE activities
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;`
+    , Object.values(fields));
+
+    return activity;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
